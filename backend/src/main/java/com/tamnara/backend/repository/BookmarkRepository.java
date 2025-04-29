c0ef6a7f8a8f7e7a458ade9afd6bdd7fd1c96964
chulsu0012 <chulsu0012@gmail.com>
2025-04-29 19:33:31 +0900

    feat: 북마크 기능 리포지토리 생성 (#4)
    생성 파일 목록
    - /repository/BookmarkRepository.java

---
+++ backend/src/main/java/com/tamnara/backend/repository/BookmarkRepository.java
@@ -0,0 +1,9 @@
+package com.tamnara.backend.repository;
+
+import com.tamnara.backend.domain.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+}
