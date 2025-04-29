86f42708c496f87c1e2ff1697ed374a9d82badb9
chulsu0012 <chulsu0012@gmail.com>
2025-04-29 19:26:28 +0900

    feat: 북마크 기능 도메인 생성 (#4)
    생성 파일 목록
    - /domain/Bookmark.java: bookmark 테이블
    
    참고사항
    - Bookmark 도메인에서 외래키 관련 속성(user, news) 주석 처리

---
+++ backend/src/main/java/com/tamnara/backend/domain/Bookmark.java
@@ -0,0 +1,34 @@
+package com.tamnara.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "bookmark", indexes = @Index(name = "idx_user_id_id_desc", columnList = "user_id, id DESC"))
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //    @ManyToOne(fetch = FetchType.LAZY)
+    //    @JoinColumn(name = "user_id", referencedColumnName = "id", updatable = false)
+    //    @OnDelete(action = OnDeleteAction.SET_NULL)
+    //    private User user;
+
+    //    @ManyToOne(fetch = FetchType.LAZY)
+    //    @JoinColumn(name = "news_id", referencedColumnName = "id", nullable = false, updatable = false)
+    //    @OnDelete(action = OnDeleteAction.CASCADE)
+    //    private News news;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
