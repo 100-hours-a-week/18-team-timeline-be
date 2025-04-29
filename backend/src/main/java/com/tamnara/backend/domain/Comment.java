494a1d387cb8db1bb0d0ed20761c5ebc05522860
chulsu0012 <chulsu0012@gmail.com>
2025-04-29 17:45:00 +0900

    feat: 댓글 기능 도메인 생성 (#3)
    생성 파일 목록
    - /domain/Comment.java: comments 테이블
    
    참고사항
    - Comment 도메인에서 외래키 관련 속성(user, news) 주석 처리

---
+++ backend/src/main/java/com/tamnara/backend/domain/Comment.java
@@ -0,0 +1,39 @@
+package com.tamnara.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "comments", indexes = @Index(name = "idx_news_id_id_desc", columnList = "news_id, id DESC"))
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", referencedColumnName = "id", updatable = false)
+//    @OnDelete(action = OnDeleteAction.SET_NULL)
+//    private User user;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "news_id", referencedColumnName = "id", nullable = false, updatable = false)
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    private News news;
+
+    @Column(name = "content", length = 150, nullable = false)
+    String content;
+
+    @CreatedDate
+    @Column(name="created_at", nullable = false, updatable = false)
+    LocalDateTime createdAt;
+}
