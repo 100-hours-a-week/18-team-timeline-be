19c98b7a911dbc7e46949dca22043ee7f75dd62a
chulsu0012 <chulsu0012@gmail.com>
2025-04-30 10:46:12 +0900

    test: 뉴스 카테고리 리포지토리 테스트에 따른 기능 추가 및 수정 (#2)
    #### 테스트 목록
    - /news/RepositoryTest/CategoryRepositoryTest.java
      - 단일 생성
      - 단일 삭제
      - num ASC 정렬하여 전체 조회
    
    #### 도메인 수정사항
    - /news/domain/Category.java
      - name 타입의 크기 10 -> 20(ENTERTAINMENT = 13자)
      - 인덱스 idx_num 설정 추가
      - enum CategoryType 생성 -> name에 적용
    
    #### 리포지토리 수정사항
    - /news/repository/CategoryRepository.java
      - 인덱스(num) 추가
      - findAll()에 num ASC 정렬 조건 추가

--- backend/src/main/java/com/tamnara/backend/news/domain/Category.java
+++ backend/src/main/java/com/tamnara/backend/news/domain/Category.java
@@ -9,14 +9,15 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Setter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "categories")
+@Table(name = "categories", indexes = @Index(name = "idx_name", columnList = "name"))
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 10, unique = true)
-    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", nullable = false, length = 20, unique = true)
+    private CategoryType name;
 
     @Column(name = "num", nullable = false, unique = true)
     private Long num = 0L;
--- backend/src/main/java/com/tamnara/backend/news/repository/CategoryRepository.java
+++ backend/src/main/java/com/tamnara/backend/news/repository/CategoryRepository.java
@@ -2,8 +2,16 @@ package com.tamnara.backend.news.repository;
 
 import com.tamnara.backend.news.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("""
+        SELECT c FROM Category c
+        ORDER BY c.num ASC
+    """)
+    List<Category> findAll();
 }
