94f762448d240c0bcb2a94fb00c4b7bd109a0df7
chulsu0012 <chulsu0012@gmail.com>
2025-04-30 11:16:06 +0900

    test: 뉴스 기능의 뉴스 이미지 리포지토리 테스트에 따른 뉴스 이미지 기능 추가 및 수정 (#2)
    #### 테스트 목록
    - /news/RepositoryTest/NewsImageRepositoryTest.java
      - 단일 이미지 생성
      - 단일 이미지 업데이트
      - 특정 뉴스 관련 단일이미지 조회
      - 단일 이미지 삭제
    
    #### 리포지토리 수정사항
    - /news/repository/NewsImageRepository.java
      - 특정 뉴스와 관련된 단일 이미지 조회 메서드 추가

--- backend/src/main/java/com/tamnara/backend/news/repository/NewsImageRepository.java
+++ backend/src/main/java/com/tamnara/backend/news/repository/NewsImageRepository.java
@@ -4,6 +4,9 @@ import com.tamnara.backend.news.domain.NewsImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NewsImageRepository extends JpaRepository<NewsImage, Long> {
+    Optional<NewsImage> findByNewsId(Long newsId);
 }
