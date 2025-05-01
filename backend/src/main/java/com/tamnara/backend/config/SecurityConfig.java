b773dfc7f34a264d8082229e0913d74b7710b265
Haein <haeinjo@postech.ac.kr>
2025-05-01 19:50:35 +0900

    chore: rollback SecurityConfig.java

--- backend/src/main/java/com/tamnara/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/tamnara/backend/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
                                 "/webjars/**",
                                 "/auth/**"  // 로그인, 회원가입 등은 인증 없이 허용
                         ).permitAll()
-                        .anyRequest().authenticated() // 나머지는 인증 필요
+//                        .anyRequest().authenticated() // 나머지는 인증 필요
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtProvider, userDetailsService),
