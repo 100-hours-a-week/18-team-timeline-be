61beb3b08bbd60cdd09994f55416d29a44a9ee46
chulsu0012 <chulsu0012@gmail.com>
2025-05-02 11:40:29 +0900

    feat: 댓글 서비스에 뉴스 존재 여부 검증에 따른 예외 처리 로직 추가 (#9)

--- backend/src/main/java/com/tamnara/backend/comment/service/CommentServiceImpl.java
+++ backend/src/main/java/com/tamnara/backend/comment/service/CommentServiceImpl.java
@@ -4,10 +4,13 @@ import com.tamnara.backend.comment.domain.Comment;
 import com.tamnara.backend.comment.dto.CommentCreateRequest;
 import com.tamnara.backend.comment.dto.CommentDTO;
 import com.tamnara.backend.comment.repository.CommentRepository;
+import com.tamnara.backend.news.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
 
+    private final NewsRepository newsRepository;
     private final CommentRepository commentRepository;
 
     @Override
     public List<CommentDTO> getComments(Long newsId, Integer page, Integer size) {
+        if (!newsRepository.existsById(newsId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "요청하신 뉴스가 존재하지 않습니다.");
+        }
         Page<Comment> comments = commentRepository.findAllByNewsId(newsId, PageRequest.of(page, size));
 
         List<CommentDTO> commentDTOS = new ArrayList<>();
@@ -37,6 +44,10 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public Long save(Long newsId, CommentCreateRequest commentCreateRequest) {
+        if (!newsRepository.existsById(newsId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "요청하신 뉴스가 존재하지 않습니다.");
+        }
+
         Comment comment = new Comment();
         comment.setContent(commentCreateRequest.getContent());
         return commentRepository.save(comment).getId();
