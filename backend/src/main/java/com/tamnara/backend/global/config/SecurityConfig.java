f363bc47c4cb6ff0f52d4941d9fa6cd8bd48b087
chulsu0012 <chulsu0012@gmail.com>
2025-05-03 03:18:01 +0900

    refactor: 불필요한 security 파일 제거 및 SecurityConfig 정리

--- backend/src/main/java/com/tamnara/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/tamnara/backend/global/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.tamnara.backend.global.config;
 
-import com.tamnara.backend.global.security.JwtAccessDeniedHandler;
-import com.tamnara.backend.global.security.JwtAuthenticationHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationHandler jwtAuthenticationHandler;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -34,14 +29,11 @@ public class SecurityConfig {
                                         "/v3/api-docs/**",
                                         "/swagger-resources/**",
                                         "/webjars/**",
-                                        "/auth/**"  // 로그인, 회원가입 등은 인증 없이 허용
+                                        "/auth/**",  // 로그인, 회원가입 등은 인증 없이 허용
+                                        "/news/**"
                                 ).permitAll()
                                 .anyRequest().authenticated() // 나머지는 인증 필요
 //                        .anyRequest().permitAll()
-                )
-                .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint(jwtAuthenticationHandler)
-                        .accessDeniedHandler(jwtAccessDeniedHandler)
                 )
                 .build();
     }
