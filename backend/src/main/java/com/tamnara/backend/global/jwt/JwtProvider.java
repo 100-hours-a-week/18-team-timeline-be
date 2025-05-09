74dd7e0ce9e002dfa4e5c16591f3046ad22ffd8a
Haein <haeinjo@postech.ac.kr>
2025-05-09 13:55:32 +0900

    refactor: username -> name in jwt

--- backend/src/main/java/com/tamnara/backend/global/jwt/JwtProvider.java
+++ backend/src/main/java/com/tamnara/backend/global/jwt/JwtProvider.java
@@ -34,6 +34,7 @@ public class JwtProvider {
         return Jwts.builder()
                 .subject(user.getId().toString())
                 .claim("role", user.getRole().toString())
+                .claim("name", user.getUsername())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY))
                 .signWith(secretKey)
