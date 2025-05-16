20f1035b88a1903d0eef913244c693e0fb854fad
chulsu0012 <chulsu0012@gmail.com>
2025-05-16 11:19:54 +0900

    fix: getNormalNewsCardPage 오버로딩 제거 및 카테고리 조회 시 ECT 분기 처리 분리 (#57)

--- backend/src/main/java/com/tamnara/backend/news/service/NewsService.java
+++ backend/src/main/java/com/tamnara/backend/news/service/NewsService.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public interface NewsService {
     List<NewsCardDTO> getHotissueNewsCardPage(Long userId);
     List<NewsCardDTO> getNormalNewsCardPage(Long userId, Integer page, Integer size);
-    List<NewsCardDTO> getNormalNewsCardPage(Long userId, String category, Integer page, Integer size);
+    List<NewsCardDTO> getNormalNewsCardPageByCategory(Long userId, String category, Integer page, Integer size);
     Map<String, List<NewsCardDTO>> getNormalNewsCardPages(Long userId, Integer page, Integer size);
     NewsDetailResponse getNewsDetail(Long newsId, Long userId);
     NewsDetailResponse save(Long userId, boolean isHotissue, NewsCreateRequest req);
