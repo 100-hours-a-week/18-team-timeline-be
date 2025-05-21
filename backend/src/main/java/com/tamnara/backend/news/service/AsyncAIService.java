c7ee6969f5a004c46d5c7ebfba969c76c28e0fc4
chulsu0012 <chulsu0012@gmail.com>
2025-05-21 11:49:42 +0900

    feat: 비동기 처리용 AsyncAIService 클래스 생성 및 @EnableAsync 어노테이션 추가로 비동기 처리 기능 활성화 (#69)
    - 메인 클래스에 @EnableAsync 어노테이션 설정
    - AsyncAIService 클래스 추가 및 @async 어노테이션 설정
    - AsyncAIService 클래스로 여론 통계 생성 API 요청 로직 이동
    - 여론 통계 생성 API 요청에 404 응답 시 null 반환 로직 추가

---
+++ backend/src/main/java/com/tamnara/backend/news/service/AsyncAIService.java
@@ -0,0 +1,11 @@
+package com.tamnara.backend.news.service;
+
+import com.tamnara.backend.news.dto.StatisticsDTO;
+import com.tamnara.backend.news.dto.WrappedDTO;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface AsyncAIService {
+    CompletableFuture<WrappedDTO<StatisticsDTO>> getAIStatisticsDTO(String endpoint, List<String> keywords, Integer num);
+}
---
+++ backend/src/main/java/com/tamnara/backend/news/service/AsyncAIServiceImpl.java
@@ -0,0 +1,51 @@
+package com.tamnara.backend.news.service;
+
+import com.tamnara.backend.global.exception.AIException;
+import com.tamnara.backend.news.dto.StatisticsDTO;
+import com.tamnara.backend.news.dto.WrappedDTO;
+import com.tamnara.backend.news.dto.request.AIStatisticsRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class AsyncAIServiceImpl implements AsyncAIService {
+
+    private final WebClient aiWebClient;
+
+    @Async
+    public CompletableFuture<WrappedDTO<StatisticsDTO>> getAIStatisticsDTO(String endpoint, List<String> keywords, Integer num) {
+        AIStatisticsRequest req = new AIStatisticsRequest(
+                keywords,
+                num
+        );
+
+        WrappedDTO<StatisticsDTO> res = aiWebClient.post()
+                .uri(endpoint)
+                .bodyValue(req)
+                .retrieve()
+                .onStatus(
+                        status -> status == HttpStatus.NOT_FOUND,
+                        clientResponse -> Mono.empty()
+                )
+                .onStatus(
+                        HttpStatusCode::isError,
+                        clientResponse -> clientResponse
+                                .bodyToMono(new ParameterizedTypeReference<WrappedDTO<StatisticsDTO>>() {})
+                                .flatMap(errorBody -> Mono.error(new AIException(errorBody)))
+                )
+                .bodyToMono(new ParameterizedTypeReference<WrappedDTO<StatisticsDTO>>() {})
+                .block();
+
+        return CompletableFuture.completedFuture(res);
+    }
+}
