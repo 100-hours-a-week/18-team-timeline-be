1fc8e909da9ed8951bcd51d01f78885122337633
Haein <haeinjo@postech.ac.kr>
2025-05-22 16:34:46 +0900

    refactor: dto name made consistent && new dto applied to test codes

--- backend/src/main/java/com/tamnara/backend/user/dto/UserUpdateRequest.java
+++ backend/src/main/java/com/tamnara/backend/user/dto/UserUpdateRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class UserUpdateRequestDto {
+public class UserUpdateRequest {
 
     @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
     private String nickname;
