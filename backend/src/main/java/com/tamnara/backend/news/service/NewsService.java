224e033b29dded513f53f7222ebb461f8082512c
chulsu0012 <chulsu0012@gmail.com>
2025-05-26 13:12:03 +0900

    refactor: getMultiCategoryPage와 getSingleCategoryPage 메서드의 정의 순서 변경 (#131)

--- backend/src/main/java/com/tamnara/backend/news/service/NewsService.java
+++ backend/src/main/java/com/tamnara/backend/news/service/NewsService.java
@@ -7,8 +7,8 @@ import com.tamnara.backend.news.dto.response.category.MultiCategoryResponse;
 
 public interface NewsService {
     HotissueNewsListResponse getHotissueNewsCardPage();
-    Object getSingleCategoryPage(Long userId, String category, Integer offset);
     MultiCategoryResponse getMultiCategoryPage(Long userId, Integer offset);
+    Object getSingleCategoryPage(Long userId, String category, Integer offset);
     NewsDetailDTO getNewsDetail(Long newsId, Long userId);
     NewsDetailDTO save(Long userId, boolean isHotissue, NewsCreateRequest req);
     NewsDetailDTO update(Long newsId, Long userId);
