d6ba741409d1209a4fc2de7be6e9ef3d171e9805
Haein <haeinjo@postech.ac.kr>
2025-05-27 17:49:23 +0900

    test: Add TestUtils for setting private fields via reflection

--- backend/src/test/java/com/tamnara/backend/auth/utils/TestUtils.java
+++ backend/src/test/java/com/tamnara/backend/auth/utils/TestUtils.java
@@ -1,4 +1,13 @@
 package com.tamnara.backend.auth.utils;
 
 public class TestUtils {
+    public static void setPrivateField(Object target, String fieldName, Object value) {
+        try {
+            var field = target.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(target, value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
