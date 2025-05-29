0ee44706d266e00c456933683fa6a23ba686f0fc
chulsu0012 <chulsu0012@gmail.com>
2025-05-29 11:08:37 +0900

    test: NewsTagRepository 테스트 추가 (#145)
    - 뉴스태그 생성 성공 검증
    - 뉴스태그의 뉴스 필드 null 불가 검증
    - 뉴스태그의 태그 필드 null 불가 검증
    - 뉴스태그의 뉴스와 태그 업데이트 불가 검증
    - 뉴스태그 삭제 성공 검증

---
+++ backend/src/test/java/com/tamnara/backend/news/repository/NewsTagRepositoryTest.java
@@ -0,0 +1,157 @@
+package com.tamnara.backend.news.repository;
+
+import com.tamnara.backend.global.config.JpaConfig;
+import com.tamnara.backend.news.domain.News;
+import com.tamnara.backend.news.domain.NewsTag;
+import com.tamnara.backend.news.domain.Tag;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+@Import(JpaConfig.class)
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class NewsTagRepositoryTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired private NewsTagRepository newsTagRepository;
+    @Autowired private TagRepository tagRepository;
+    @Autowired private NewsRepository newsRepository;
+
+    private NewsTag createNewsTag(News news, Tag tag) {
+        NewsTag newsTag = new NewsTag();
+        newsTag.setNews(news);
+        newsTag.setTag(tag);
+        return newsTag;
+    }
+
+    News news;
+    Tag tag;
+
+    @BeforeEach
+    void setUp() {
+        news = new News();
+        news.setTitle("제목");
+        news.setSummary("미리보기 내용");
+        newsRepository.saveAndFlush(news);
+
+        tag = new Tag();
+        tag.setName("태그");
+        tagRepository.saveAndFlush(tag);
+    }
+
+    @Test
+    void 뉴스태그_생성_성공_검증() {
+        // given
+        NewsTag newsTag = createNewsTag(news, tag);
+        newsTagRepository.saveAndFlush(newsTag);
+
+        // when
+        NewsTag findNewsTag = newsTagRepository.findById(newsTag.getId()).get();
+
+        // then
+        assertEquals(newsTag.getId(), findNewsTag.getId());
+    }
+
+    @Test
+    void 뉴스태그_뉴스_필드_null_불가_검증() {
+        // given
+        NewsTag newsTag = new NewsTag();
+
+        // when
+        newsTag.setTag(tag);
+
+        // then
+        assertNull(newsTag.getNews());
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            newsTagRepository.saveAndFlush(newsTag);
+        });
+    }
+
+    @Test
+    void 뉴스태그_태그_필드_null_불가_검증() {
+        // given
+        NewsTag newsTag = new NewsTag();
+
+        // when
+        newsTag.setNews(news);
+
+        // then
+        assertNull(newsTag.getTag());
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            newsTagRepository.saveAndFlush(newsTag);
+        });
+    }
+
+    @Test
+    void 뉴스태그_뉴스와_태그_업데이트_불가_검증() {
+        // given
+        NewsTag newsTag = createNewsTag(news, tag);
+        newsTagRepository.saveAndFlush(newsTag);
+
+        // when
+        News newNews = new News();
+        newNews.setTitle("제목");
+        newNews.setSummary("미리보기 요약");
+        newsRepository.saveAndFlush(newNews);
+
+        Tag newTag = new Tag();
+        newTag.setName("태그2");
+        tagRepository.saveAndFlush(newTag);
+
+        NewsTag findNewsTag = newsTagRepository.findById(newsTag.getId()).get();
+        findNewsTag.setNews(newNews);
+        findNewsTag.setTag(newTag);
+        newsTagRepository.saveAndFlush(newsTag);
+
+        // then
+        assertEquals(newNews, findNewsTag.getNews());
+        assertEquals(newTag, findNewsTag.getTag());
+    }
+
+    @Test
+    void 뉴스태그_삭제_성공_검증() {
+        // given
+        NewsTag newsTag = createNewsTag(news, tag);
+        newsTagRepository.saveAndFlush(newsTag);
+
+        // when
+        NewsTag findNewsTag = newsTagRepository.findById(newsTag.getId()).get();
+        newsTagRepository.delete(findNewsTag);
+
+        // then
+        assertFalse(newsTagRepository.existsById(newsTag.getId()));
+    }
+
+    @Test
+    void 뉴스_ID로_연관된_뉴스태그들_조회_검증() {
+        // given
+        NewsTag newsTag1 = createNewsTag(news, tag);
+        newsTagRepository.saveAndFlush(newsTag1);
+        NewsTag newsTag2 = createNewsTag(news, tag);
+        newsTagRepository.saveAndFlush(newsTag2);
+
+        // when
+        List<NewsTag> findNewsTags = newsTagRepository.findByNewsId(news.getId());
+
+        // then
+        assertEquals(2, findNewsTags.size());
+    }
+}
