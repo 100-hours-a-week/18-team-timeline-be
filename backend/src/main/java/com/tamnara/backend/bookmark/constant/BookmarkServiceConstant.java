69c8f30047415c5c86d5f665d063b897acf2e8c2
chulsu0012 <chulsu0012@gmail.com>
2025-06-02 15:46:18 +0900

    feat: 북마크 상수 클래스 추가 및 페이징 상수 정의 (#214)
    - BookmarkServiceConstant 클래스 추가
    - 상수 클래스에 PAGE_SIZE 상수 정의

---
+++ backend/src/main/java/com/tamnara/backend/bookmark/constant/BookmarkServiceConstant.java
@@ -0,0 +1,7 @@
+package com.tamnara.backend.bookmark.constant;
+
+public final class BookmarkServiceConstant {
+    private BookmarkServiceConstant() {}
+
+    public static final Integer PAGE_SIZE = 20;
+}
