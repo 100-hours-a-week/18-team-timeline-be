484d409e8b15ac81b8be79e27facc26535fb65dc
Haein <haeinjo@postech.ac.kr>
2025-06-04 21:20:54 +0900

    chore: unnecessary comments deleted

--- backend/src/main/java/com/tamnara/backend/poll/repository/VoteRepository.java
+++ backend/src/main/java/com/tamnara/backend/poll/repository/VoteRepository.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-    // 특정 유저가 특정 투표에 대해 남긴 모든 투표 기록
     List<Vote> findByUserIdAndPollId(Long userId, Long pollId);
 
-    // 특정 Poll의 모든 투표 기록
     List<Vote> findByPollId(Long pollId);
 }
