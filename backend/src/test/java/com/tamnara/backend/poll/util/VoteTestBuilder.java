98ee7d1982e9f275ac6d703f3bf4fe6c265ee142
Haein <haeinjo@postech.ac.kr>
2025-06-05 15:49:27 +0900

    feat: util classes for building poll option and vote

--- backend/src/test/java/com/tamnara/backend/poll/util/VoteTestBuilder.java
+++ backend/src/test/java/com/tamnara/backend/poll/util/VoteTestBuilder.java
@@ -1,4 +1,21 @@
 package com.tamnara.backend.poll.util;
 
+import com.tamnara.backend.poll.domain.Poll;
+import com.tamnara.backend.poll.domain.PollOption;
+import com.tamnara.backend.poll.domain.Vote;
+import com.tamnara.backend.user.domain.User;
+
+import java.time.LocalDateTime;
+
 public class VoteTestBuilder {
+
+    public static Vote build(User user, Poll poll, PollOption option) {
+        return Vote.builder()
+                .user(user)
+                .poll(poll)
+                .option(option)
+                .createdAt(LocalDateTime.now())
+                .votedAt(LocalDateTime.now())
+                .build();
+    }
 }
