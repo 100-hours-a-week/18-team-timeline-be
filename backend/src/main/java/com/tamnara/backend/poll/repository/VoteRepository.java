d36a1525187cdc302523208f7ad89fb6e938f72a
Haein <haeinjo@postech.ac.kr>
2025-06-05 10:55:00 +0900

    feat: automatic scheduling for updating vote stat

--- backend/src/main/java/com/tamnara/backend/poll/repository/VoteRepository.java
+++ backend/src/main/java/com/tamnara/backend/poll/repository/VoteRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
-
     List<Vote> findByUserIdAndPollId(Long userId, Long pollId);
-
     List<Vote> findByPollId(Long pollId);
+    long countByPollIdAndOptionId(Long pollId, Long optionId);
 }
