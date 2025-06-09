9eb8d74f214f219b2a74c757b88ea951142c4277
chulsu0012 <chulsu0012@gmail.com>
2025-06-09 13:37:55 +0900

    feat: 스케줄링 서비스 처리 시간 측정 로그 추가 (#257)

--- backend/src/main/java/com/tamnara/backend/news/service/NewsSchedulerServiceImpl.java
+++ backend/src/main/java/com/tamnara/backend/news/service/NewsSchedulerServiceImpl.java
@@ -21,8 +21,12 @@ public class NewsSchedulerServiceImpl implements NewsSchedulerService {
     public void createHotissueNews() {
         try {
             log.info("[INFO] 핫이슈 뉴스 생성 시작");
+            Long start = System.currentTimeMillis();
+
             newsService.createHotissueNews();
-            log.info("[INFO] 핫이슈 뉴스 생성 완료");
+
+            Long end = System.currentTimeMillis();
+            log.info("[INFO] 핫이슈 뉴스 생성 완료: {} ms", (end - start));
         } catch (Exception e) {
             log.error("[ERROR] 핫이슈 뉴스 생성 중 오류 발생: {}", e.getMessage(), e);
         }
@@ -34,8 +38,12 @@ public class NewsSchedulerServiceImpl implements NewsSchedulerService {
     public void deleteOldNewsAndOrphanTags() {
         try {
             log.info("[INFO] 오래된 뉴스 삭제 및 고아 태그 삭제 시작");
+            Long start = System.currentTimeMillis();
+
             newsService.deleteOldNewsAndOrphanTags();
-            log.info("[INFO] 오래된 뉴스 삭제 및 고아 태그 삭제 완료");
+
+            Long end = System.currentTimeMillis();
+            log.info("[INFO] 오래된 뉴스 삭제 및 고아 태그 삭제 완료: {}", (end - start));
         } catch (Exception e) {
             log.error("[ERROR] 오래된 뉴스 삭제 및 고아 태그 삭제 중 오류 발생: {}", e.getMessage(), e);
         }
