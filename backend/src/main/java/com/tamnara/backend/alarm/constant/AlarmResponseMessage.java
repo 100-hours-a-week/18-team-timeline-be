6c184031a5605c0319b05390b44ef6be4ea18c58
chulsu0012 <chulsu0012@gmail.com>
2025-06-09 15:16:19 +0900

    fix: 알림 확인 응답 메시지 변경 (#273)

--- backend/src/main/java/com/tamnara/backend/alarm/constant/AlarmResponseMessage.java
+++ backend/src/main/java/com/tamnara/backend/alarm/constant/AlarmResponseMessage.java
@@ -5,7 +5,7 @@ public final class AlarmResponseMessage {
 
     // 알림 성공 메시지
     public static final String ALARM_FETCH_SUCCESS = "요청하신 알림 목록을 성공적으로 불러왔습니다.";
-    public static final String ALARM_CHECK_SUCCESS = "알림 확인에 성공하였습니다.";
+    public static final String ALARM_CHECK_SUCCESS = "알림을 성공적으로 확인하였습니다.";
 
     // 알림 예외 메시지
     public static final String ALARM_NOT_FOUND = "존재하지 않는 알림입니다.";
