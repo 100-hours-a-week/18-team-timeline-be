0cbc30f49c5e227dd341e5cf05ae8a44a2979dfc
chulsu0012 <chulsu0012@gmail.com>
2025-06-09 11:07:26 +0900

    test: UserAlarmRepository 단위 테스트 추가 (#272)
    - 회원알림 확인 여부를 조회 검증 제거
    - 북마크 알림 목록 조회 시 페이징과 ID 내림차순 정렬과 타겟 종류 검증 추가

--- backend/src/test/java/com/tamnara/backend/alarm/repository/UserAlarmRepositoryTest.java
+++ backend/src/test/java/com/tamnara/backend/alarm/repository/UserAlarmRepositoryTest.java
@@ -3,8 +3,12 @@ package com.tamnara.backend.alarm.repository;
 import com.tamnara.backend.alarm.domain.Alarm;
 import com.tamnara.backend.alarm.domain.AlarmType;
 import com.tamnara.backend.alarm.domain.UserAlarm;
+import com.tamnara.backend.bookmark.domain.Bookmark;
+import com.tamnara.backend.bookmark.repository.BookmarkRepository;
 import com.tamnara.backend.global.config.JpaConfig;
 import com.tamnara.backend.global.config.QuerydslConfig;
+import com.tamnara.backend.news.domain.News;
+import com.tamnara.backend.news.repository.NewsRepository;
 import com.tamnara.backend.user.domain.Role;
 import com.tamnara.backend.user.domain.State;
 import com.tamnara.backend.user.domain.User;
@@ -44,11 +48,14 @@ public class UserAlarmRepositoryTest {
     @PersistenceContext private EntityManager em;
 
     @Autowired private UserAlarmRepository userAlarmRepository;
-    @Autowired private UserRepository userRepository;
     @Autowired private AlarmRepository alarmRepository;
+    @Autowired private UserRepository userRepository;
+    @Autowired private NewsRepository newsRepository;
+    @Autowired private BookmarkRepository bookmarkRepository;
 
     User user;
     Alarm alarm;
+    News news;
 
     @BeforeEach
     void setUp() {
@@ -77,7 +84,7 @@ public class UserAlarmRepositoryTest {
         em.clear();
     }
 
-    private UserAlarm createUserAlarm(boolean isChecked) {
+    private UserAlarm createUserAlarm(Alarm alarm, boolean isChecked) {
         UserAlarm userAlarm = new UserAlarm();
         userAlarm.setIsChecked(isChecked);
         userAlarm.setCheckedAt(isChecked ? LocalDateTime.now() : null);
@@ -86,10 +93,35 @@ public class UserAlarmRepositoryTest {
         return userAlarm;
     }
 
+    private Alarm createAlarm(String title, String content, String targetType, Long targetId) {
+        Alarm alarm = new Alarm();
+        alarm.setTitle(title);
+        alarm.setContent(content);
+        alarm.setTargetType(targetType == null ? null : AlarmType.valueOf(targetType));
+        alarm.setTargetId(targetId);
+
+        return alarmRepository.saveAndFlush(alarm);
+    }
+
+    private News createNews() {
+        news = new News();
+        news.setTitle("제목");
+        news.setSummary("미리보기 내용");
+        news.setIsHotissue(true);
+        return newsRepository.saveAndFlush(news);
+    }
+
+    private Bookmark createBookmark(News news) {
+        Bookmark bookmark = new Bookmark();
+        bookmark.setUser(user);
+        bookmark.setNews(news);
+        return bookmarkRepository.saveAndFlush(bookmark);
+    }
+
     @Test
     void 회원알림_생성_성공_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(false);
+        UserAlarm userAlarm = createUserAlarm(alarm, false);
         userAlarmRepository.save(userAlarm);
 
         // when
@@ -102,7 +134,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원알림_확인_여부_null_불가_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(true);
+        UserAlarm userAlarm = createUserAlarm(alarm, true);
 
         // when
         userAlarm.setIsChecked(null);
@@ -116,7 +148,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원알림_확인_시간_null_가능_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(true);
+        UserAlarm userAlarm = createUserAlarm(alarm, true);
 
         // when
         userAlarm.setCheckedAt(null);
@@ -131,7 +163,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원알림_회원_필드_null_불가_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(true);
+        UserAlarm userAlarm = createUserAlarm(alarm, true);
 
         // when
         userAlarm.setUser(null);
@@ -145,7 +177,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원알림_알림_필드_null_불가_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(true);
+        UserAlarm userAlarm = createUserAlarm(alarm, true);
 
         // when
         userAlarm.setAlarm(null);
@@ -159,7 +191,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원알림_확인_상태_업데이트_성공_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(false);
+        UserAlarm userAlarm = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm);
         em.clear();
 
@@ -180,7 +212,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원알림_삭제_성공_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(false);
+        UserAlarm userAlarm = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm);
         em.clear();
 
@@ -194,7 +226,7 @@ public class UserAlarmRepositoryTest {
     @Test
     void 단일_회원알림_조회_성공_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(false);
+        UserAlarm userAlarm = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm);
         em.clear();
 
@@ -208,15 +240,15 @@ public class UserAlarmRepositoryTest {
     @Test
     void 회원_ID로_회원_알림_목록_조회_시_페이징과_ID_내림차순_정렬_검증() {
         // given
-        UserAlarm userAlarm1 = createUserAlarm(false);
+        UserAlarm userAlarm1 = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm1);
-        UserAlarm userAlarm2 = createUserAlarm(false);
+        UserAlarm userAlarm2 = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm2);
-        UserAlarm userAlarm3 = createUserAlarm(false);
+        UserAlarm userAlarm3 = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm3);
-        UserAlarm userAlarm4 = createUserAlarm(false);
+        UserAlarm userAlarm4 = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm4);
-        UserAlarm userAlarm5 = createUserAlarm(false);
+        UserAlarm userAlarm5 = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm5);
         em.clear();
 
@@ -235,27 +267,59 @@ public class UserAlarmRepositoryTest {
     }
 
     @Test
-    void 회원알림_확인_여부를_조회_검증() {
-        // given
-        UserAlarm userAlarm1 = createUserAlarm(false);
+    void 북마크_알림_목록_조회_시_페이징과_ID_내림차순_정렬과_타겟_종류_검증() {
+        News news1 = createNews();
+        News news2 = createNews();
+        em.clear();
+
+        Bookmark bookmark1 = createBookmark(news1);
+        Bookmark bookmark2 = createBookmark(news2);
+
+        Alarm alarm1 = createAlarm("제목1", "내용1", AlarmType.NEWS.toString(), bookmark1.getNews().getId());
+        Alarm alarm2 = createAlarm("제목2", "내용2", AlarmType.NEWS.toString(), bookmark2.getNews().getId());
+        Alarm alarm3 = createAlarm("제목3", "내용3", AlarmType.NEWS.toString(), bookmark1.getNews().getId());
+        Alarm alarm4 = createAlarm("제목4", "내용4", AlarmType.NEWS.toString(), bookmark2.getNews().getId());
+        Alarm alarm5 = createAlarm("제목5", "내용5", AlarmType.NEWS.toString(), bookmark1.getNews().getId());
+        em.clear();
+
+        UserAlarm userAlarm1 = createUserAlarm(alarm1, false);
         userAlarmRepository.saveAndFlush(userAlarm1);
-        UserAlarm userAlarm2 = createUserAlarm(true);
+        UserAlarm userAlarm2 = createUserAlarm(alarm2, false);
         userAlarmRepository.saveAndFlush(userAlarm2);
+        UserAlarm userAlarm3 = createUserAlarm(alarm3, false);
+        userAlarmRepository.saveAndFlush(userAlarm3);
+        UserAlarm userAlarm4 = createUserAlarm(alarm4, false);
+        userAlarmRepository.saveAndFlush(userAlarm4);
+        UserAlarm userAlarm5 = createUserAlarm(alarm5, false);
+        userAlarmRepository.saveAndFlush(userAlarm5);
         em.clear();
 
         // when
-        Boolean result1 = userAlarmRepository.existsByIdAndIsCheckedTrue(userAlarm1.getId());
-        Boolean result2 = userAlarmRepository.existsByIdAndIsCheckedTrue(userAlarm2.getId());
+        int pageSize = 3;
+        Pageable pageable = PageRequest.of(0, pageSize);
+        Page<UserAlarm> bookmarkAlarmPage = userAlarmRepository.findBookmarkAlarms(user.getId(), pageable);
+        List<UserAlarm> bookmarkAlarmList = bookmarkAlarmPage.getContent();
 
         // then
-        assertFalse(result1);
-        assertTrue(result2);
+        assertEquals(5, bookmarkAlarmPage.getTotalElements());
+        assertEquals(pageSize, bookmarkAlarmList.size());
+        assertEquals(userAlarm5.getId(), bookmarkAlarmList.get(0).getId());
+        assertEquals(userAlarm4.getId(), bookmarkAlarmList.get(1).getId());
+        assertEquals(userAlarm3.getId(), bookmarkAlarmList.get(2).getId());
+
+        assertEquals(AlarmType.NEWS, userAlarm5.getAlarm().getTargetType());
+        assertEquals(AlarmType.NEWS, userAlarm4.getAlarm().getTargetType());
+        assertEquals(AlarmType.NEWS, userAlarm3.getAlarm().getTargetType());
+
+        assertEquals(userAlarm5.getAlarm().getTargetId(), bookmark1.getNews().getId());
+        assertEquals(userAlarm4.getAlarm().getTargetId(), bookmark2.getNews().getId());
+        assertEquals(userAlarm3.getAlarm().getTargetId(), bookmark1.getNews().getId());
     }
 
     @Test
     void 확인하지_않은_회원알림을_확인_처리_검증() {
         // given
-        UserAlarm userAlarm = createUserAlarm(false);
+        UserAlarm userAlarm = createUserAlarm(alarm, false);
         userAlarmRepository.saveAndFlush(userAlarm);
         em.clear();
 
