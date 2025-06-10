353f0bba5cd0ba6feced8a13a8809815d9ae1b5a
chulsu0012 <chulsu0012@gmail.com>
2025-06-10 11:45:31 +0900

    cherry-pick: 뉴스 기능 리포지토리 테스트의 @Import 목록을 TestConfig로 변경 (#301)

--- backend/src/test/java/com/tamnara/backend/news/repository/CategoryRepositoryTest.java
+++ backend/src/test/java/com/tamnara/backend/news/repository/CategoryRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.tamnara.backend.news.repository;
 
-import com.tamnara.backend.global.config.JpaConfig;
-import com.tamnara.backend.global.config.QuerydslConfig;
+import com.tamnara.backend.config.TestConfig;
 import com.tamnara.backend.news.domain.Category;
 import com.tamnara.backend.news.domain.CategoryType;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-@Import({JpaConfig.class, QuerydslConfig.class})
+@Import(TestConfig.class)
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CategoryRepositoryTest {
--- backend/src/test/java/com/tamnara/backend/news/repository/NewsTagRepositoryTest.java
+++ backend/src/test/java/com/tamnara/backend/news/repository/NewsTagRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.tamnara.backend.news.repository;
 
-import com.tamnara.backend.global.config.JpaConfig;
-import com.tamnara.backend.global.config.QuerydslConfig;
+import com.tamnara.backend.config.TestConfig;
 import com.tamnara.backend.news.domain.News;
 import com.tamnara.backend.news.domain.NewsTag;
 import com.tamnara.backend.news.domain.Tag;
@@ -24,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
-@Import({JpaConfig.class, QuerydslConfig.class})
+@Import(TestConfig.class)
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class NewsTagRepositoryTest {
