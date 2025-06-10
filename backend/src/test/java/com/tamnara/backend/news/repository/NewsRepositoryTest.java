c81264ffb3a8d63bc6677d1d5b5954a3aeb68994
chulsu0012 <chulsu0012@gmail.com>
2025-06-10 11:45:31 +0900

    refactor: 뉴스 기능 리포지토리 테스트의 @Import 목록을 TestConfig로 변경 (#301)

--- backend/src/test/java/com/tamnara/backend/news/repository/NewsRepositoryTest.java
+++ backend/src/test/java/com/tamnara/backend/news/repository/NewsRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.tamnara.backend.news.repository;
 
-import com.tamnara.backend.global.config.JpaConfig;
-import com.tamnara.backend.global.config.QuerydslConfig;
+import com.tamnara.backend.config.TestConfig;
 import com.tamnara.backend.news.constant.NewsServiceConstant;
 import com.tamnara.backend.news.domain.Category;
 import com.tamnara.backend.news.domain.CategoryType;
@@ -42,7 +41,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-@Import({JpaConfig.class, QuerydslConfig.class})
+@Import(TestConfig.class)
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class NewsRepositoryTest {
